984599938fc9cdb3112b354aaa118c05120e89fe
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-06-21 09:42:24 +0700

    018 004

--- 018/004/main.go
+++ 018/004/main.go
@@ -12,6 +12,12 @@ type menu struct {
 	Dinner    []string
 }
 
+type restaurant struct {
+	Name    string
+	Address string
+	Menu    menu
+}
+
 var tpl *template.Template
 
 func init() {
@@ -25,7 +31,13 @@ func main() {
 		Dinner:    []string{"Nasi Goreng", "Sate"},
 	}
 
-	err := tpl.Execute(os.Stdout, m)
+	r := restaurant{
+		Name:    "Warung Burjo",
+		Address: "Mabes",
+		Menu:    m,
+	}
+
+	err := tpl.Execute(os.Stdout, r)
 
 	if err != nil {
 		log.Fatalln(err)
