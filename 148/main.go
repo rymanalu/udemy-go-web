1787cb8acd43712126b36e54e809a9c63f56f3f6
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-07-29 17:03:35 +0700

    148

---
+++ 148/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type book struct {
+	isbn   string
+	title  string
+	author string
+	price  float32
+}
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://manalu:@localhost/udemy_bookstore?sslmode=disable")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("You connected to your database")
+
+	rows, err := db.Query("select * from books")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	bks := make([]book, 0)
+
+	for rows.Next() {
+		bk := book{}
+
+		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+
+		if err != nil {
+			panic(err)
+		}
+
+		bks = append(bks, bk)
+	}
+
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+
+	for _, bk := range bks {
+		fmt.Printf("%s, %s, %s, $%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
+	}
+}
